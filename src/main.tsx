770043f3ef9f3a7447356a9952068f636f8f4d31
Niral <niral@niralmehta.org>
2024-02-25 16:47:53 +0000

    chore: possible routing fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import App from "./App.tsx";
 import "./index.css";
 import { About } from "./Pages/About";
-import { Navigation } from "./components/Navigation/index.tsx";
-import { Footer } from "./components/Footer/index.tsx";
 import { aboutPath, rootPath } from "./components/Navigation/routes.ts";
+import { Home } from "./Pages/Home/index.ts";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: `/${rootPath}`,
     element: <App />,
-  },
-  {
-    path: `/${aboutPath}`,
-    element: <About />,
+    children: [
+      {
+        path: `/${rootPath}`,
+        element: <Home />,
+      },
+      {
+        path: `/${aboutPath}`,
+        element: <About />,
+      },
+    ],
   },
   {},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="min-h-screen flex flex-col w-full overflow-hidden">
-      <Navigation />
-      <RouterProvider router={router} />
-      <Footer />
-    </div>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
