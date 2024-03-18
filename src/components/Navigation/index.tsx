0400e11fcab0c14dad7f5654548156a80b75318b
Niral Mehta <niral@niralmehta.org>
2024-03-18 17:41:49 +0000

    chore: update about page (#41)

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -2,7 +2,7 @@ import { Links } from "./Links";
 
 export const Navigation = () => {
   return (
-    <div className="flex flex-row justify-end m-10 z-10">
+    <div className="flex flex-row justify-center lg:justify-end m-10 z-10">
       <ul className="flex justify-between">
         <Links />
       </ul>
