1c1249476654ffed108a0cbdccc70ff808d524a5
Niral <niral@niralmehta.org>
2024-03-18 17:41:26 +0000

    chore: update about page

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,7 +1,11 @@
 import { SVGIcon } from "@/types";
 
-import AtLogo from "../../assets/at.svg?react";
-import SinWaveAlt from "../../assets/sign-wave-alt.svg?react";
+import Curve from "../../assets/curve.svg?react";
+
+import BoardingPass from "../../assets/boarding_pass.svg?react";
+import Books from "../../assets/books.svg?react";
+import Controller from "../../assets/controller.svg?react";
+import Me from "../../assets/me.svg?react";
 
 interface Fact {
   className?: string;
@@ -25,31 +29,38 @@ export const About = (): JSX.Element => {
     {
       text: "books i've read",
       quantity: 394,
-      Icon: AtLogo,
+      Icon: Books,
     },
     {
       text: "video games i've completed",
       quantity: 67,
-      Icon: AtLogo,
+      Icon: Controller,
     },
     {
       text: "countries i've visited",
       quantity: 7,
-      Icon: AtLogo,
+      Icon: BoardingPass,
     },
   ];
 
   return (
     <div className="relative">
-      <SinWaveAlt className="absolute -left-[5%] top-0 z-10 w-[110%]" />
-      <div className="relative mb-[10rem] lg:mb-[15rem]">
-        <div className="bg-[#FF83EC] mt-[45%] text-white px-10">
-          <div className="flex flex-col items-center text-center">
-            <h1 className="uppercase text-2xl lg:text-5xl">
-              Welcome to my world of <br />
-              color and play
-            </h1>
-            <AtLogo className="h-[100px] w-[100px] lg:h-[400px] lg:w-[400px]" />
+      <Curve className="absolute rotate-[180deg] -z-10 top-[18rem]" />
+      <div className="mb-[10rem] lg:mb-[15rem]">
+        <div className="flex flex-col w-full justify-center items-center">
+          <Me className="h-[300px] lg:h-[425px]" />
+          <p className="mt-2">pleasure to meet you!</p>
+        </div>
+        <div className="bg-[#FF83EC] text-white px-10 mt-[10rem] lg:mt-[20rem]">
+          <div className="lg:w-1/2 lg:mt-[2rem]  mb-[3rem] lg:mb-[5rem] ">
+            <p>
+              i am a graphic designer with a focus on book design and design
+              experiences. my passions lie in mental health, feminism within
+              design and creating a world for my audience to escape into. i am
+              fuelled by exploration, experimentation and play and i find
+              inspiration in the many interests i hold within my personal life
+              which include reading, gaming and fitness.
+            </p>
           </div>
 
           <div className="flex flex-col">
@@ -57,26 +68,15 @@ export const About = (): JSX.Element => {
               some fun <br />
               facts about me:
             </p>
-            <div className="self-end">
+            <div className="self-end lg:pr-[12rem] mb-[4rem]">
               {facts.map((fact) => (
                 <Fact {...fact} />
               ))}
             </div>
           </div>
-
-          <div className="lg:w-1/2 mt-[2rem] mb-0">
-            <p>
-              i am a graphic and digital designer with a focus on book design
-              and design experiences. my passions lie in mental health, feminism
-              within design and creating a world for my audience to escape into.
-              i am fuelled by exploration, experimentation and play and i find
-              inspiration in the many interests i hold within my personal life
-              which include reading, gaming and fitness.
-            </p>
-          </div>
-          <div className="w-[200%] h-[120%] rounded-bl-full rounded-br-full bg-[#FF83EC] absolute -left-[50%] -bottom-[10rem] xl:-bottom-[16rem] -z-10" />
         </div>
       </div>
+      <Curve className="absolute rotate-[180deg] -z-0 bottom-[4rem] lg:-bottom-[5rem] text-white" />
     </div>
   );
 };
